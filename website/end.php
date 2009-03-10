b760841f3391c0848899850b3e26f6a9454dec48
snelg <snelg>
2009-03-10 16:12:26 +0000

    New SF logo

--- website/end.php
+++ website/end.php
@@ -1,5 +1,4 @@
 </div>
-<a class="sfLogo" href="http://sourceforge.net">
-<img class="sfLogo" src="http://sourceforge.net/sflogo.php?group_id=49979&amp;type=1" width="88" height="31" border="0" alt="SourceForge.net Logo"/ ></a>
+<a href="http://sourceforge.net/projects/mp3gain"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=49979&type=13" width="120" height="30" border="0" alt="Get MP3Gain at SourceForge.net. Fast, secure and Free Open Source software downloads" /></a>
 </body>
 </html>
