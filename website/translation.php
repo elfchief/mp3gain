e392a4528b72bf91ac9d995e0a2b6288650c874b
snelg <snelg>
2011-06-20 05:14:09 +0000

    Website should show that new help file exists...

--- website/translation.php
+++ website/translation.php
@@ -91,6 +91,7 @@ Pick a language, and just un-zip the file into your MP3Gain folder:
 	<li><a href="help/mp3gain-deutsch.zip">German</a></li>
 	<li><a href="help/mp3gain-japanese.zip">Japanese</a></li>
 	<li><a href="help/mp3gain-nederlands.zip">Nederlands</a></li>
+	<li><a href="help/mp3gain-brasil.zip">Brazilian Portuguese</a></li>
     <li><a href="help/mp3gain-russian.zip">Russian</a></li>
     <li><a href="help/mp3gain-espanol.zip">Spanish</a></li>
 	<li><a href="help/mp3gain-turkce.zip">Turkish</a></li>
